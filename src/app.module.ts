c47e4862e1b654bfffb6189ee22202fca3a68a32
Now <now.thongtangsai@gmail.com>
2022-11-05 12:29:50 +0700

    BE: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './models/prisma/prisma.module';
 import { UsersModule } from './models/users/users.module';
+import { AuthModule } from './authentication/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
